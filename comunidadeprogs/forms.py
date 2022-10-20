caf63352375f2b58b439158959de2c570aa454e2
leoFagundes <leofagundes2015@gmail.com>
2022-10-20 13:09:27 -0300

    update perfil

--- comunidadeprogs/forms.py
+++ comunidadeprogs/forms.py
@@ -1,8 +1,10 @@
 #instalar o falsk_wtf -> pip install flask-wtf
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeprogs.models import Usuario
+from flask_login import current_user
 
 
 class FormCriarConta(FlaskForm):
@@ -30,3 +32,17 @@ class FormLogin(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
+
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()]) #DataRequired -> obrigatório
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png', 'gif', 'svg'])])
+    botao_submit_editarperfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        #verificar se ele mudou de email
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com esse e-mail. Cadastre outro e-mail')
--- comunidadeprogs/routes.py
+++ comunidadeprogs/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, url_for, request, flash, redirect
 from comunidadeprogs import app, database, bcrypt
-from comunidadeprogs.forms import FormCriarConta, FormLogin
+from comunidadeprogs.forms import FormCriarConta, FormLogin, FormEditarPerfil
 from comunidadeprogs.models import Usuario
 from flask_login import login_user, logout_user, current_user, login_required
-
+import secrets
+import os
+from PIL import Image #pip install Pillow
 
 lista_usuarios = ['Leo', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -77,5 +79,40 @@ def criar_post():
     return render_template('criarpost.html')
 
 
+def salvar_imagem(imagem):
+    #adicionar um codigo aleatorio no nome da imagem
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
 
+    #reduzir o tamanho da imagem
+    tamanho = (400, 400)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+
+    # salvar a imagem na pasta fotos_perfil
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
+    imagem_reduzida.save(caminho_completo)
+
+    return nome_arquivo
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        if form.foto_perfil.data:
+            #mudar o campo foto_perfil do usuario para o novo nome da imagem
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+        database.session.commit()
+        flash(f'Perfil atualizado com Sucesso', "alert-success")
+        return redirect(url_for('perfil'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
 
